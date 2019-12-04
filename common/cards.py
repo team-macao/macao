cfe3e7af7392752277cf5c460c6ea9f642bd169a
MKC <mateuszkarolchrusciel@gmail.com>
2019-12-04 15:32:36 +0100

    + Possibility to choose the number of players + Created "macao.py" file - the main file of the game implementation + Call to the "macao.main" function, when chosen a new game in the "launcher" + Generation of the main deck + Generation of the players' "hands" from a randomly selected cards, from the main deck, per num of players + Printing of the last card on the table + Printing the "hands" of each player in a loop + Some smaller changes, like:  + Removed "()" at functions definitions in the file "launcher"  + Moved definition of variables to the top of the "cards" file

--- common/cards.py
+++ common/cards.py
@@ -1,4 +1,8 @@
-class Card:
+suits = ("Hearts", "Diamonds", "Clubs", "Spades",)
+ranks = ("Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two")
+
+
+class Card(object):
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
@@ -12,122 +16,87 @@ class Card:
 
 class Ace(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class King(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Queen(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Jack(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Ten(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Nine(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Eight(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Seven(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Six(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Five(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Four(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Three(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
 class Two(Card):
     def __init__(self, suit, rank):
-        Card.__init__(self, suit, rank)
+        super().__init__(suit, rank)
 
 
-def deck():
-    def generate_cards(suit):
-        temp_deck = []
-
-        for rank in ranks:
-            if rank == "Ace":
-                temp_deck.append(Ace(suit, rank))
-            elif rank == "King":
-                temp_deck.append(King(suit, rank))
-            elif rank == "Queen":
-                temp_deck.append(Queen(suit, rank))
-            elif rank == "Jack":
-                temp_deck.append(Jack(suit, rank))
-            elif rank == "Ten":
-                temp_deck.append(Ten(suit, rank))
-            elif rank == "Nine":
-                temp_deck.append(Nine(suit, rank))
-            elif rank == "Eight":
-                temp_deck.append(Eight(suit, rank))
-            elif rank == "Seven":
-                temp_deck.append(Seven(suit, rank))
-            elif rank == "Six":
-                temp_deck.append(Six(suit, rank))
-            elif rank == "Five":
-                temp_deck.append(Five(suit, rank))
-            elif rank == "Four":
-                temp_deck.append(Four(suit, rank))
-            elif rank == "Three":
-                temp_deck.append(Three(suit, rank))
-            elif rank == "Two":
-                temp_deck.append(Two(suit, rank))
-            else:
-                raise Exception
-
-        return temp_deck
-
+def generate_deck():
     deck = []
 
     for suit in suits:
         if suit == "Hearts":
-            hearts_deck = generate_cards(suit)
+            hearts_deck = generate_cards_per_suit(suit)
             for card in hearts_deck:
                 deck.append(card)
         elif suit == "Diamonds":
-            diamonds_deck = generate_cards(suit)
+            diamonds_deck = generate_cards_per_suit(suit)
             for card in diamonds_deck:
                 deck.append(card)
         elif suit == "Clubs":
-            clubs_deck = generate_cards(suit)
+            clubs_deck = generate_cards_per_suit(suit)
             for card in clubs_deck:
                 deck.append(card)
         elif suit == "Spades":
-            spades_deck = generate_cards(suit)
+            spades_deck = generate_cards_per_suit(suit)
             for card in spades_deck:
                 deck.append(card)
         else:
@@ -136,6 +105,37 @@ def deck():
     return deck
 
 
-suits = ("Hearts", "Diamonds", "Clubs", "Spades",)
+def generate_cards_per_suit(suit):
+    temp_deck = []
 
-ranks = ("Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two")
+    for rank in ranks:
+        if rank == "Ace":
+            temp_deck.append(Ace(suit, rank))
+        elif rank == "King":
+            temp_deck.append(King(suit, rank))
+        elif rank == "Queen":
+            temp_deck.append(Queen(suit, rank))
+        elif rank == "Jack":
+            temp_deck.append(Jack(suit, rank))
+        elif rank == "Ten":
+            temp_deck.append(Ten(suit, rank))
+        elif rank == "Nine":
+            temp_deck.append(Nine(suit, rank))
+        elif rank == "Eight":
+            temp_deck.append(Eight(suit, rank))
+        elif rank == "Seven":
+            temp_deck.append(Seven(suit, rank))
+        elif rank == "Six":
+            temp_deck.append(Six(suit, rank))
+        elif rank == "Five":
+            temp_deck.append(Five(suit, rank))
+        elif rank == "Four":
+            temp_deck.append(Four(suit, rank))
+        elif rank == "Three":
+            temp_deck.append(Three(suit, rank))
+        elif rank == "Two":
+            temp_deck.append(Two(suit, rank))
+        else:
+            raise Exception
+
+    return temp_deck
