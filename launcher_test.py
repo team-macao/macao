92d2334a6f9039c65adcdb036452e3e2d91ec0e5
MKC <mateuszkarolchrusciel@gmail.com>
2019-12-12 18:41:37 +0100

    minimal cosmetic changes

--- launcher_test.py
+++ launcher_test.py
@@ -6,7 +6,7 @@ def test_print_menu(capsys):
     obj._print_menu()
     captured = capsys.readouterr()
     pattern = '\nMacao\n\n' \
-              '1. Continue the last game\n' \
+              '1. Continue last game\n' \
               '2. New game\n' \
               '3. Exit game\n'
     assert captured.out == pattern
--- macao.py
+++ macao.py
@@ -16,11 +16,11 @@ class Game:
         self.players = []
         self.table = []
 
-        # generate players (class instances) for number of players inputted
+        # generate players (class instances) for number of players entered
         for num in range(1, self.num_of_players + 1):
             self.players.append(Player(num))
 
-        # for each player, draw 5 inital cards to hand
+        # for each player, draw 5 initial cards to hand
         for player in self.players:
             player.draw_initial_hand(self.deck)
 
@@ -48,31 +48,31 @@ class Game:
                 for card in player.hand:
                     card_index += 1
                     print(f"{card_index}. {repr(card)}")
-                # set flag "users_selection_valid" to False
-                users_selection_valid = False
+                # set flag "users_selection_is_valid" to False
+                users_selection_is_valid = False
                 # iterate until got a valid user's selection
-                while not users_selection_valid:
+                while not users_selection_is_valid:
                     # get user's selection
-                    users_selection = input("\nChoose a card to be put on the table: ")
+                    users_selection = input("\nChoose a card to be put on table: ")
                     # check if user entered a number
                     try:
                         users_selection_int = int(users_selection)
                     # if unsuccessful, inform user to enter a number instead
                     except ValueError:
-                        # inform user to input a number
+                        # inform user to enter a number
                         print("Invalid input! Try entering a number instead")
                     # check if selected card index is correct
                     else:
                         # if user selected a "out-of-range" index
                         if users_selection_int not in range(1, card_index + 1):
-                            # inform user to input a valid number
+                            # inform user to enter a valid number
                             print("Invalid selection! Selected number out of range!")
                         # if user selected correctly
                         else:
                             # lay out selected card from user's hand on the table
                             self.table.append(player.hand.pop(users_selection_int - 1))
-                            # set flag "users_selection_valid" to True
-                            users_selection_valid = True
+                            # set flag "users_selection_is_valid" to True
+                            users_selection_is_valid = True
 
 
 class Player:
@@ -85,7 +85,7 @@ class Player:
             self.hand.append(deck.pool.pop(random.randint(0, len(deck.pool) - 1)))
         return self.hand
 
-    def draw_a_card(self):
+    def draw_card(self):
         pass
 
     def lay_out_card(self, cards_index, player, table):
